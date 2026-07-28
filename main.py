fa67109fd4941c5c6ab3c382faa39b76520b5a32
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-07-28 19:34:36 -0300

    feat: learning basic config of fastapi

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
+from routes import auth_router, order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
 # Comando para rodar com uvicorn - uv run uvicorn main:app --reload
---
+++ routes/__init__.py
@@ -0,0 +1,4 @@
+from .auth_routes import auth_router
+from .orders_routes import order_router
+
+__all__ = ["auth_router", "order_router"]
