400de70707a63f13d76940ac88660d72ec2ee8f5
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-07-30 22:31:29 -0300

    feat: create all models

--- models/models.py
+++ models/models.py
@@ -1,8 +1,18 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy import declarative_base
+from enum import auto
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    String,
+    Boolean,
+    ForeignKey,
+    Float,
+)
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
 
 # Cria a conexão com o banco
-db = create_engine("127.0.0.1:3306//banco.db")
+db = create_engine("127.0.0.1:3306//learning.db")
 
 
 # Cria a base do banco de dados
@@ -10,6 +20,10 @@ Base = declarative_base()
 
 
 # Cria as tabelas e classes do banco de dados
+
+# User
+
+
 class User(Base):
     __tablename__ = "users"
 
@@ -26,6 +40,52 @@ class User(Base):
         self.password = password
         self.active = active
         self.is_admin = is_admin
+
+
+# Order
+
+
+class Order(Base):
+    __tablename__ = "orders"
+
+    STATUS_PEDIDOS = (
+        ("PENDING", "PENDING"),
+        ("FINISH", "FINISH"),
+        ("CANCELLED", "CANCELLED"),
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column(
+        "status", ChoiceType(choices=STATUS_PEDIDOS)
+    )  # FINISH, CANCELLED, PENDING
+    user = Column("user", ForeignKey("users.id"))
+    price = Column("price", Float)
+
+    def __init__(self, user, status="PENDING", price=0):
+        self.user = user
+        self.status = status
+        self.price = price
+
+
+# ItemOrder
+
+
+class OrderItem(Base):
+    __table__ = "orderitems"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    quantity = Column("quantity", Integer)
+    flavor = Column("flavor", String)
+    bulk = Column("bulk", String)
+    unit_price = Column("unit_proce", Float)
+    order = Column("order", ForeignKey("orders.id"))
+
+    def __init__(self, quantity, flavor, bulk, unit_price, order):
+        self.quantity = quantity
+        self.flavor = flavor
+        self.bulk = bulk
+        self.unit_price = unit_price
+        self.order = order
 
 
 # Executa a criação do banco de dados
