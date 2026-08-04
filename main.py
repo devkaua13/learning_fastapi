a017b9dbb9bdc751a816862182b37f3ded8c91ae
devkaua13 <kaua.vinicios_2005@hotmail.com>
2026-08-04 15:45:38 -0300

    feat: load secret key of env

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from routes import auth_router, order_router
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 app = FastAPI()
-
 
 app.include_router(auth_router)
 app.include_router(order_router)
